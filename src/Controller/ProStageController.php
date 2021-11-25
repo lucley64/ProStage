85c29d2ae293fc43e933cd7b9d569f2d7ddf4a44
LEYDERT Luc <lleydert@iutbayonne.univ-pau.fr>
2021-11-25 11:58:45 +0100

    Mise en place du controlleur metier

---
+++ src/Controller/ProStageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProStageController extends AbstractController
+{
+    /**
+     * @Route("/pro/stage", name="pro_stage")
+     */
+    public function index(): Response
+    {
+        return $this->render('pro_stage/index.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+}
