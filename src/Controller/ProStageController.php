93e73f741620ac24fbb10ac7433b309ac5963683
LEYDERT Luc <lleydert@iutbayonne.univ-pau.fr>
2022-01-13 17:28:52 +0100

    Finalisation de la page détail entreprise

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Stage;
 use App\Entity\Entreprise;
+use App\Entity\Formation;
 use App\Repository\EntrepriseRepository;
+use App\Repository\FormationRepository;
 use App\Repository\StageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -40,34 +43,60 @@ class ProStageController extends AbstractController
     /**
      * @Route("/formations", name="ProStage_formations")
      */
-    public function afficherFormations(): Response
+    public function afficherFormations(FormationRepository $reposFormation): Response
     {
+        $formations = $reposFormation->findAll();
 
         return $this->render(
-            'pro_stage/afficherFormations.html.twig'
+            'pro_stage/afficherFormations.html.twig',
+            ['formations' => $formations]
         );
     }
 
     /**
      * @Route("/stages/{id}", name="ProStage_stage")
      */
-    public function afficherDetailStage($id): Response
+    public function afficherDetailStage($id, StageRepository $reposStage): Response
     {
+
+        $stage = $reposStage->find($id);
 
         return $this->render(
             'pro_stage/affichageDetailStage.html.twig',
-            ['idRessource' => $id]
+            ['stage' => $stage]
         );
     }
 
     /**
-     * @Route("/entreprise/{id}", name="ProStage_detail_entreprise")
+     * @Route("/entreprises/{id}", name="ProStage_detail_entreprise")
      */
-    public function afficherDetailEntreprise($id)
+    public function afficherDetailEntreprise($id, EntrepriseRepository $reposEntrep): Response
     {
+        $entreprise = $reposEntrep->find($id);
+
+        $stages = $this->getDoctrine()->getRepository(Stage::class)->findByEntreprise($entreprise);
+
         return $this->render(
             'pro_stage/affichageDetailEntreprise.html.twig',
-            ['id' => $id]
+            [
+                'entreprise' => $entreprise,
+                'stages' => $stages
+            ]
+        );
+    }
+
+    /**
+     * @Route("/formation/{id}", name="ProStage_detail_formation")
+     */
+    public function afficherDetailFormation($id, FormationRepository $reposFormation): Response
+    {
+
+        $formation = $reposFormation->find($id);
+
+        
+
+        return $this->render(
+            'pro_stage/affichageDetailFormation.html.twig',
         );
     }
 }
