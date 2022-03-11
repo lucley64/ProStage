4a6d2e888e1fe15bfd82af5f43db1f5c90814a06
LEYDERT Luc <lleydert@iutbayonne.univ-pau.fr>
2022-03-11 14:20:09 +0100

    Création d'un formulaire de connexion

--- src/Security/FormulaireLoginAuthenticator.php
+++ src/Security/FormulaireLoginAuthenticator.php
@@ -96,7 +96,7 @@ class FormulaireLoginAuthenticator extends AbstractFormLoginAuthenticator implem
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('ProStage_accueil'));
     }
 
     protected function getLoginUrl()
